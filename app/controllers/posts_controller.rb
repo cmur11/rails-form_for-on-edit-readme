0d573d0f5a12128f12fef9338a8838408059a2f3
Conor Murnane <conormurnane11@gmail.com>
2020-12-15 09:01:14 -0500

    weird error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,10 +12,11 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new
-	  @post.title = params[:title]
-	  @post.description = params[:description]
-	  @post.save
+		post_params = params.require(:post).permit(:title,:description)
+	  	@post = Post.create(post_params)
+	#   @post.title = params[:title]
+	#   @post.description = params[:description]
+	#   @post.save
 	  redirect_to post_path(@post)
 	end
 
@@ -25,7 +26,8 @@ class PostsController < ApplicationController
 
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(title: params[:title], description: params[:description])
+	  post_params = params.require(:post).permit(:title,:description)
+	  @post.update(post_params)
 	  redirect_to post_path(@post)
 	end
 end
